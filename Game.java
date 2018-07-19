3b429db7f593bdc2d9d144544b3adf001887113e
Faizan Wajid <faiz.wajid@gmail.com>
2018-07-18 22:16:19 -0400

    Java Games and other Exercises

---
+++ Game.java
@@ -0,0 +1,119 @@
+public class Game {
+
+  /* Method to print the map, and update with new player location.
+   * px = x position
+   * py = y position
+   * showMap = display the map or not
+   */
+  public static String printMap(int px, int py, boolean showMap){
+    // Map Legend (dimensions: 10 by 10)
+    // # : lake
+    // ^ : trap
+    // $ : prize
+    String[][] map = {
+      {" | "," - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " | "},
+      {" | "," $ ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " | "},
+      {" | "," - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " | "},
+      {" | "," - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " | "},
+      {" | "," - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " | "},
+      {" | "," - ", " - ", " - ", " - ", " - ", " - ", " - ", " W ", " | "},
+      {" | "," - ", " # ", " # ", " - ", " - ", " - ", " - ", " - ", " | "},
+      {" | "," # ", " # ", " # ", " - ", " - ", " - ", " ^ ", " ^ ", " | "},
+      {" | "," # ", " # ", " # ", " - ", " - ", " - ", " ^ ", " ^ ", " | "},
+      {" | "," - ", " - ", " - ", " - ", " - ", " - ", " - ", " ^ ", " | "}
+    };
+
+    // Store the map length for later use
+    int m = map.length;
+
+    // Shift (x,y) to cartesian coords
+    py = py + (m/2);
+    px = px + (m/2);
+
+    // Store the symbol at the desired location
+    String s = map[py][px];
+
+    // If no issues, update the map with new position
+    map[py][px] = " o ";
+    
+    // Show map if requested (method param)
+    if(showMap == true){
+      for(int i = 0; i < m; i++){
+        for(int j = 0; j < m; j++){
+          System.out.print(map[i][j]);
+        }
+        System.out.println();
+      }
+    }
+
+    // Return the symbol found at desired location
+    return s;
+  }
+
+  public static int guessingGame(){
+    int secretNumber;
+    int maxNum = 99;
+    secretNumber = (int) (Math.random() * maxNum + 1);           
+    int guess;
+    int tries = 0;
+
+    do {
+      System.out.print("Enter a guess (1-"+(maxNum+1)+"):");
+      guess = IO.readInt();
+
+      if (guess == secretNumber){
+        System.out.println("Your guess is correct. Congratulations!");
+      } else if (guess < secretNumber) {
+        System.out.println("Your guess is smaller than the secret number.");
+      } else if (guess > secretNumber) {
+        System.out.println("Your guess is greater than the secret number.");
+      }
+      tries++;
+    } while (guess != secretNumber);
+
+    System.out.println("Number of tries: "+tries);
+    return tries;
+  }
+
+  public static void main(String[] args){
+    // (x,y) of the player
+    int pos_x = 0;
+    int pos_y = 0;
+
+    System.out.println("Welcome to ______, a weird place.");
+    System.out.println("A strange wizard at the local market place");
+    System.out.println("gave you this map. You only know that the");
+    System.out.println("# means LAKE, ^ means TRAP. What could $ be?");
+    System.out.println("");
+    
+    while(true)
+    {
+      printMap(pos_x, pos_y, true);
+      System.out.println("You can go: {up, down, left, right}");
+      String input = IO.readString();
+      if(input.equalsIgnoreCase("up")){
+        pos_y--;
+      }else if(input.equalsIgnoreCase("down")){
+        pos_y++;
+      }else if(input.equalsIgnoreCase("right")){
+        pos_x++;
+      }else if(input.equalsIgnoreCase("left")){
+        pos_x--;
+      }
+      if(printMap(pos_x, pos_y, false).equals(" # ")){
+        System.out.println("You fell into the lake!");
+        System.out.println("Game Over!");
+        return;
+      }
+      if(printMap(pos_x, pos_y, false).equals(" W ")){
+        int tries = guessingGame();
+        if ( tries > 10 ){
+          System.out.println("You're a loser!");
+          return;
+        }
+      }
+    }
+
+  }
+}
+
