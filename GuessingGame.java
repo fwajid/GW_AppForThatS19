43b97315ec8fa2ade07d1c39c0cd2ce33e3b3150
Faizan Wajid <faiz.wajid@gmail.com>
2019-07-15 19:43:16 -0400

    adding guessing game

---
+++ GuessingGame.java
@@ -0,0 +1,19 @@
+public class GuessingGame {
+
+  public static void main(String[] args) {
+
+    int secretNumber;
+    secretNumber = (int) (Math.random() * 999 + 1);           
+    int tries = 0;
+
+    do {
+      System.out.print("Enter a guess (1-1000): ");
+      /* Your Code Here */
+
+    } while(false);
+
+    System.out.println("\nNumber of tries: "+tries);
+
+  }
+}
+
